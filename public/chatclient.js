9d6c9adb9c57d43f88651941c5392017b796ac81
Ubuntu <ubuntu@ip-172-31-46-168.us-west-2.compute.internal>
2014-03-09 06:55:56 +0000

    Added chatroom support, username support, chatroom participant lists

--- public/chatclient.js
+++ public/chatclient.js
@@ -1,16 +1,72 @@
 window.onload = function() {
    var socket = io.connect("http://54.186.49.63:8080/");
-   var chatDiv = document.getElementById("chatDiv");
+   var chatDiv = document.getElementById("chatMessages");
    var messageInput = document.getElementById("messageInput");
+   var joinChatRoom = document.getElementById("joinChatRoom");
    var submitMessage = document.getElementById("submitMessage");
+   var partipicants = document.getElementById("participants");
+
+   var currentChatroom;
+   var currentUsername;
+   var currentParticipants;   
+
    submitMessage.onclick = sendMessage;
    function sendMessage() {
-      socket.emit('receiveMessageFromClient', { message : messageInput.value });
+      socket.emit('sendMessage', { message : messageInput.value, user : currentUsername, chatroom : currentChatroom });
       messageInput.value = "";
+   }
+
+   joinChatRoom.onclick = regUserJoinChat;
+   function regUserJoinChat() {
+      var chatrooms = document.getElementsByName("chatroom");
+      var user = document.getElementById("username");
+      if (user.value != null && user.value.length > 0) {
+         for (var i = 0; i < chatrooms.length; i++) {
+            var room = chatrooms[i];
+            if (room.checked == true) {
+               socket.emit('newUser', { username : user.value, chatroom : room.value } );
+               currentChatroom = room.value;
+               currentUsername = user.value;
+               break;
+            }
+         }
+      }
    } 
-   socket.on('sendMessageToClient', function(data) {
+
+   function updateParticipants() {
+      participants.innerHTML = "";
+      for (var i = 0; i < currentParticipants.length; i++) {
+         participants.innerHTML += currentParticipants[i] + "<br>";
+      }
+   }
+
+   socket.on('receiveMessage', function(data) {
       if (data.message) {
-         chatDiv.innerHTML = chatDiv.innerHTML + "<br>" + data.message;
+         chatDiv.innerHTML = chatDiv.innerHTML + "<br>" + data.user + ": " + data.message;
       }
    });
+
+   socket.on('joinChatroom', function(data) {
+      currentParticipants = data.participants;
+      updateParticipants();
+      document.getElementById("chatWindow").style.display = 'block';
+   });
+
+   socket.on('userLeft', function(data) {
+      participants.innerHTML = "";
+      for (var i = 0; i < currentParticipants.length; i++) {
+         if (data.user === currentParticipants[i]) {
+            currentParticipants.splice(i, 1);
+         } else {
+            participants.innerHTML += currentParticipants[i] + "<br>";
+         }
+      }
+   });
+
+   socket.on('userJoined', function(data) {
+      participants.innerHTML += data.username + "<br>";
+   });
+   window.onbeforeunload = function() {
+      socket.disconnect();
+   };
 }
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require("http");
-var filesys = require('fs');
+var filesys = require("fs");
 var url = require("url");
-var path = require('path');
-var mime = require('mime');
+var path = require("path");
+var mime = require("mime");
+
+var clients = [];
 
 var app = http.createServer(function(request, response) {
    var reqFile = url.parse(request.url).pathname;
@@ -26,8 +28,64 @@ app.listen(8080);
 var socketsIO = require('socket.io').listen(app);
 
 socketsIO.sockets.on('connection', function(socket) {
-   socket.emit('sendMessageToClient', { message : "Welcome to the chat server" });
-   socket.on('receiveMessageFromClient', function(data) {
-      socketsIO.sockets.emit('sendMessageToClient', data);
+   
+   socket.on('sendMessage', function(data) {
+      for (var i = 0; i < clients.length; i++) {
+         var user = clients[i];
+         if (user.chatroom === data.chatroom) {
+            socketsIO.sockets.socket(user.id).emit('receiveMessage', data);
+         }
+      }
+   });
+
+   socket.on('newUser', function(data) {
+      var user = new Object();
+      user.id = socket.id;
+      user.username = data.username;
+      user.chatroom = data.chatroom;
+
+      var participants = [];
+      for (var i = 0; i < clients.length; i++) {
+         var client = clients[i];
+         if (client.username === user.username) {
+            socket.emit('joinChatroom', {status : "Fail", message : "username taken"});
+            return;
+         } else if (client.chatroom == data.chatroom) {
+            participants.push(client.username);
+            socketsIO.sockets.socket(client.id).emit('receiveMessage', { user : "admin", message : data.username + " has joined the chat!" });
+            socketsIO.sockets.socket(client.id).emit('userJoined', { username : data.username });
+         }
+      }
+      participants.push(user.username);
+      clients.push(user);
+      socket.emit('receiveMessage', { user : "admin", message : "Welcome to the " + user.chatroom + ", " + user.username + "!<br>" 
+                                                                + user.username + " has joined the chat!"} );
+      socket.emit('joinChatroom', {status : "Success", participants : participants} );
+   });
+
+   socket.on('disconnect', function() {
+      var chatroomLeft;
+      var userLeft;
+      for (var i = 0; i < clients.length; i++) {
+         var client = clients[i];
+         if (client.id == socket.id) {
+            if (client.chatroom != null && client.username != null) {
+               chatroomLeft = client.chatroom;
+               userLeft = client.username;
+            }
+            clients.splice(i, 1);
+            break;
+         }
+      }
+      console.log(userLeft + '' + chatroomLeft);
+      if (userLeft == null || chatroomLeft == null) {
+         return;
+      } 
+      for (var q = 0; q < clients.length; q++) {
+         if (clients[q].chatroom == chatroomLeft) {
+            socketsIO.sockets.socket(clients[q].id).emit('userLeft', {user : userLeft});
+            break;
+         }
+      }
    });
 });
